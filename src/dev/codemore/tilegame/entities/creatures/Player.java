541c7549d695536fffa7b47b723f3da53deb0d5b
Boris Saric <boris.saric@gmail.com>
2015-11-28 13:55:42 +0100

    Dododan kod za animaciju pokreta

--- src/dev/codemore/tilegame/entities/creatures/Player.java
+++ src/dev/codemore/tilegame/entities/creatures/Player.java
@@ -2,13 +2,19 @@ package dev.codemore.tilegame.entities.creatures;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 import dev.codemore.tilegame.Game;
 import dev.codemore.tilegame.Handler;
+import dev.codemore.tilegame.gfx.Animation;
 import dev.codemore.tilegame.gfx.Assets;
 import dev.codemore.tilegame.gfx.GameCamera;
 
 public class Player extends Creature {
+	
+	private Animation anim_down, anim_up, anim_left, anim_right;
+	
+	
 
  public Player(Handler handler, float x, float y) {
 		super(handler, x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HIGHT);
@@ -17,10 +23,25 @@ public class Player extends Creature {
 		bounds.y = 32;
 		bounds.width = 16;
 		bounds.height = 32;
+		
+		//Animations
+		
+		anim_down = new Animation(500, Assets.player_down);
+		anim_up = new Animation(500, Assets.player_up);
+		anim_right = new Animation(500, Assets.player_right);
+		anim_left = new Animation(500, Assets.player_left);
 	}
 
 	@Override
 	public void tick() {
+		if(yMove > 0)
+			 	anim_down.tick();
+		else if(yMove < 0)
+			 	anim_up.tick();
+		else if(xMove < 0)
+			 	anim_left.tick();
+		else 
+			 	anim_right.tick();
 		 getInput();
 		 move();
 		 handler.getGameCamera().centerOnEntity(this);
@@ -42,10 +63,23 @@ public class Player extends Creature {
 
 	@Override
 	public void render(Graphics g) {
-		g.drawImage(Assets.player, (int) (x - handler.getGameCamera().getxOffset()), (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
-		
-		g.setColor(Color.RED);
-		g.fillRect((int)(x + bounds.x - handler.getGameCamera().getxOffset()), (int)(y + bounds.y - handler.getGameCamera().getyOffset()), bounds.width, bounds.height);
+		 
+			g.drawImage(getCurrentAnimationFrame() , (int) (x - handler.getGameCamera().getxOffset()), (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
+		 
+ 		//g.setColor(Color.RED);
+		//g.fillRect((int)(x + bounds.x - handler.getGameCamera().getxOffset()), (int)(y + bounds.y - handler.getGameCamera().getyOffset()), bounds.width, bounds.height);
 	}
+	
+	private BufferedImage getCurrentAnimationFrame(){
+		if(xMove < 0)
+			return anim_left.getCurrentFrame();
+		else if(xMove > 0)
+			return anim_right.getCurrentFrame();
+		else if(yMove < 0)
+			return anim_up.getCurrentFrame();
+		else  
+			return anim_down.getCurrentFrame();
+		 
+		}
 
 }
--- src/dev/codemore/tilegame/gfx/Assets.java
+++ src/dev/codemore/tilegame/gfx/Assets.java
@@ -6,17 +6,35 @@ public class Assets {
 	
 	public static final int width = 32, height = 32;
 	
-	public static BufferedImage player, dirt, grass, stone, tree;
+	public static BufferedImage dirt, grass, stone, tree;
+	
+	public static BufferedImage[] player_down, player_up, player_left, player_right;
 	
 	public static void init(){
 		 
 		SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/sheet.png"));
 		
+		player_down = new BufferedImage[2];
+		player_up = new BufferedImage[2];
+		player_left = new BufferedImage[2];
+		player_right = new BufferedImage[2];
+		
 		tree = sheet.crop(0, 0, width, height);
 		dirt = sheet.crop(width, 0, width, height);
 		grass = sheet.crop(2*width, 0, width, height);
 		stone = sheet.crop(3*width, 0, width, height);
-		player = sheet.crop(4*width, 0, width, height);
+		
+		player_down[0] = sheet.crop(4*width, 0, width, height);
+		player_down[1] = sheet.crop(5*width, 0, width, height);
+		player_up[0] = sheet.crop(6*width, 0, width, height);
+		player_up[1] = sheet.crop(7*width, 0, width, height);
+		player_right[0] = sheet.crop(4*width, height, width, height);
+		player_right[0] = sheet.crop(5*width, height, width, height);
+		player_left[0] = sheet.crop(6*width, height, width, height);
+		player_left[1] = sheet.crop(7*width, height, width, height);
+		
+	
+		//player = sheet.crop(4*width, 0, width, height);
 	}
 
 }
